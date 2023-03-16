4ff9493895c70149564a219314c0e626f558035f
forks5 <80710177+forks857@users.noreply.github.com>
2023-03-16 19:17:31 +0000

    Backup before switches over to functions

--- .ccls-cache/@home@runner@Clock/main.c
+++ .ccls-cache/@home@runner@Clock/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+
+int Steve(int *Bob,int period, int currentPeriod) {
+  while(1) {
+   if (*Bob >= period) {
+      *Bob -= period;
+      currentPeriod++;
+}  else if (*Bob < period) {
+           break;
+        }
+  } return currentPeriod;
+  }
     int main(void)
     {
-//At this point, it is a couple of seconds behind some other clocks, but I think this is more accurate. 
+//At this point, it is a couple of seconds behind some other clocks, but I think this is more accurate. updates within a millisecond, could be improved but would have to run more often and I don't yet know the impact of that. could also probably add milliseconds by just running this every millisecond and adding one
+      //Generalize some of the other bits with a function, specifically day, hour, minute, and second. other values can be returned by using pointers
+      //apparently, variables declared inside an if statement or anything else with brackets are only able to be used within that statement and its children, not its parents.
       //Time at 20:30:35 on Jan 31, 2023 -8 UTC is 1675225835, for testing purposes.
 
       const int second = 1;
@@ -13,20 +26,25 @@
       const int month[2][12] = {{2678400, 5097600, 7776000, 10368000, 13046400, 15638400, 18316800, 20995200, 23587200, 26265600, 28857600, 3153600}, {2678400, 5184000, 7862400, 10454400, 13132800, 15724800, 18403200, 21081600, 23673600, 26352000, 28944000, 31622400}};
       const int year = 31536000;
       const int leapYear = 31622400;
-      int timezone;
-      int military;
-      printf("Current timezone? (PST would be -8, for instance)\n");
-      scanf("%d", &timezone);
-      printf("Enter 1 for 24 hour clock, anything else for 12 hour clock.\n");
+      //set to PST during DST
+      int timezone = -7; 
+      int military = 0;
+      /*
+        printf("Current timezone? (PST would be -8 without DST, -7 with)\n");
+     scanf("%d", &timezone);
+     printf("Enter 1 for 24 hour clock, anything else for 12 hour clock.\n");
         scanf("%d", &military);
+      */
       while(1) {
         int hello;
         if(hello != time(NULL)) {
-        printf("\033[0;0H\033[2J");
+        printf("\033[0;0H\033[2J"); //This clears the console so it can refresh
         hello = time(NULL);
         //Bob is used to manipulate the time without touching its actual value
         int Bob = hello;
         Bob += (timezone * hour);
+        int* Joe = &Bob; //Joe is a pointer to Bob
+        printf("%p\n", Joe);
         int leapDays = 0;
         int currentYear = 1970;
         int currentLeapYear = 0;
@@ -115,7 +133,7 @@
           printf("%d\n", currentSecond);
         }
       printf("Current date is %d/%d/%d \n", currentMonth, currentDay, currentYear);
-      //Since apparently the sleep() function can only use seconds, we have to use the usleep() function, which takes microseconds.
+      //Since apparently the sleep() function can only use seconds, we have to use the usleep() function, which takes microseconds. right now, it is 1 millisecond 
       usleep(1000);
       } else {
         usleep(1000);
